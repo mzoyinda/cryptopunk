3fe0e1b92cd9263ceb0636f5f8ff89e325a33d5e
Oyindamola Dawodu <67465104+mzoyinda@users.noreply.github.com>
2022-06-19 17:37:58 +0100

    updated active punk list

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -6,7 +6,7 @@ import '../css/Main.css';
 
 
 const Main = ({selectedPunk, punkListData}) => {
-    const [activePunk, setActivePunk] = useState(punkListData[0])
+    const [activePunk, setActivePunk] = useState(punkListData.length-1)
     
     useEffect(() => {
         setActivePunk(punkListData[selectedPunk])
