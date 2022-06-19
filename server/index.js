d535f1994ba19bec3e7d320b6f91f48a6383d4fa
Oyindamola Dawodu <67465104+mzoyinda@users.noreply.github.com>
2022-06-19 17:31:36 +0100

    Updated Api

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 
 app.get('/api', (req, res) => {
   request(
-    { url: 'https://testnets-api.opensea.io/api/v1/assets?asset_contract_address=0x0c611A25e2728e7ada47812c875fe46cBf76E545&order_direction=asc' },
+    { url: 'https://testnets-api.opensea.io/api/v1/assets?asset_contract_address=0x0c611A25e2728e7ada47812c875fe46cBf76E545&order_direction=desc' },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
